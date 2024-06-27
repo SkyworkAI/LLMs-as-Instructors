ad85fb50ce51162916e81f10021bb9e7aa23d687
yingjiahao14 <1766844079@qq.com>
2024-06-27 17:14:55 +0800

    first commit

---
+++ model/train/code/utils/model/model_utils.py
@@ -0,0 +1,42 @@
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: Apache-2.0
+
+# DeepSpeed Team
+import os
+import math
+import torch
+from transformers import (
+    AutoConfig,
+    AutoModel,
+)
+
+from transformers.deepspeed import HfDeepSpeedConfig
+
+# from .reward_model import RewardModel
+
+
+def create_hf_model(model_class,
+                    model_name_or_path,
+                    tokenizer,
+                    ds_config=None,
+                    rlhf_training=False):
+    model_config = AutoConfig.from_pretrained(model_name_or_path)
+    model_config.dropout = 0.0
+    # Note: dschf is defined in function scope to avoid global effects
+    # https://huggingface.co/docs/transformers/main_classes/deepspeed#nontrainer-deepspeed-integration
+    if ds_config is not None and ds_config["zero_optimization"]["stage"] == 3:
+        dschf = HfDeepSpeedConfig(ds_config)
+    else:
+        dschf = None
+    if rlhf_training:
+        # the weight loading is handled by create critic model
+        model = model_class.from_config(model_config)
+    else:
+        model = model_class.from_pretrained(
+            model_name_or_path,
+            from_tf=bool(".ckpt" in model_name_or_path),
+            config=model_config)
+    return model
+
+
+
